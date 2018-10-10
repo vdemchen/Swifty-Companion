a5c62c790b2880cb8cc9279d01197253352b051e
Vlad Demchenko <vdemchen@gmail.com>
2018-10-10 06:10:43 +0300

    Create project structure

---
+++ source/data/database/AlamofireManager.swift
@@ -0,0 +1,9 @@
+//
+//  AlamofireManager.swift
+//  SwiftyCompanion
+//
+//  Created by Vlad Demchenko on 10/10/18.
+//  Copyright © 2018 Vlad Demchenko. All rights reserved.
+//
+
+import Foundation
---
+++ source/implementation/controllers/UserInfoViewController/UserInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserInfoViewController.swift
+//  SwiftyCompanion
+//
+//  Created by Vlad Demchenko on 10/10/18.
+//  Copyright © 2018 Vlad Demchenko. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

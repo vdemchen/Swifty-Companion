bf1f7f8522d2af86ec68c29e2c43c8ada04661a2
Vlad Demchenko <vdemchen@gmail.com>
2018-10-10 05:54:35 +0300

    Initial Commit

---
+++ SwiftyCompanion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyCompanion
+//
+//  Created by Vlad Demchenko on 10/10/18.
+//  Copyright © 2018 Vlad Demchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

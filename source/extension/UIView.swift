dc4610c894694a341780504b92b65bbde8c0fb63
Vlad Demchenko <vdemchen@gmail.com>
2018-10-25 10:47:14 +0300

    Add corner radius

---
+++ source/extension/UIView.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//MARK: - UIView extension
+
+extension UIView{
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
